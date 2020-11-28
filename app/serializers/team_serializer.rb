6bf2f11d322e97241fde8302c03a3f54e3156f2c
willanysilva <willany_lp@hotmail.com>
2020-11-27 21:18:41 -0300

    remove serializer

--- app/serializers/team_serializer.rb
+++
@@ -1,5 +0,0 @@
-class TeamSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
-  attributes :id, :name, :description, :twitter, :instagram, :github, :linkedin, :picture
-
-end
