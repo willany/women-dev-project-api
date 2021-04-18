6abc2aea045f40738eebb3549255b663307fd241
willanysilva <willany_lp@hotmail.com>
2021-04-18 14:08:28 -0300

    add basic auth admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_234934) do
+ActiveRecord::Schema.define(version: 2021_04_18_165519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_02_07_234934) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "linkedin"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
